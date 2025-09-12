6ae91b708baac9e927554c0f69926579e1d14245
Cookie_qwq <>
2025-09-12 18:39:04 +0800

    init

---
+++ src/main/kotlin/net/astrorbits/doNotDoIt/DoNotDoIt.kt
@@ -0,0 +1,14 @@
+package net.astrorbits.doNotDoIt
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DoNotDoIt : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
