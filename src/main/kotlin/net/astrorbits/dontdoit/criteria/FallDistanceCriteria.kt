6a8d1ee75003f0dd527b23b8cdf9e12a67370677
Cookie_qwq <>
2025-09-26 20:08:19 +0800

    修FallDistance

--- src/main/kotlin/net/astrorbits/dontdoit/criteria/FallDistanceCriteria.kt
+++ src/main/kotlin/net/astrorbits/dontdoit/criteria/FallDistanceCriteria.kt
@@ -2,17 +2,20 @@ package net.astrorbits.dontdoit.criteria
 
 import net.astrorbits.dontdoit.criteria.helper.CriteriaType
 import net.astrorbits.dontdoit.system.team.TeamData
-import net.astrorbits.lib.math.vector.Vec3d
-import org.bukkit.Material
 import org.bukkit.damage.DamageType
 import org.bukkit.entity.Player
+import org.bukkit.event.Listener
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.player.PlayerMoveEvent
+import java.util.UUID
 import kotlin.properties.Delegates
 
-class FallDistanceCriteria : Criteria() {
+class FallDistanceCriteria : Criteria(), Listener {
     override val type: CriteriaType = CriteriaType.FALL_DISTANCE
     var distance: Double by Delegates.notNull()
+    private val lastTickFallDistances = mutableMapOf<UUID, Float>()
+    private val lastTickYLevels = mutableMapOf<UUID, Double>()
 
     override fun readData(data: Map<String, String>) {
         super.readData(data)
@@ -21,7 +24,15 @@ class FallDistanceCriteria : Criteria() {
 
     override fun tick(teamData: TeamData) {
         for (player in teamData.members) {
-            if (player.fallDistance >= distance) {
+            val y = player.location.y
+            val uuid = player.uniqueId
+            val lastTickFallDistance = lastTickFallDistances[uuid] ?: 0f
+            val lastTickYLevel = lastTickYLevels[uuid] ?: y
+            val yLevelChange = lastTickYLevel - y
+            lastTickFallDistances[uuid] = player.fallDistance
+            lastTickYLevels[uuid] = y
+            if (lastTickFallDistance + yLevelChange >= distance) {
+                player.sendMessage("$lastTickFallDistance, $yLevelChange")
                 trigger(player)
                 break
             }
@@ -29,25 +40,19 @@ class FallDistanceCriteria : Criteria() {
     }
 
     @EventHandler
-    fun onPlayerDamaged(event: EntityDamageEvent) {  //TODO 还是有问题
-        if (event.damageSource.damageType != DamageType.FALL) return
+    fun onPlayerFallDamaged(event: EntityDamageEvent) {
+        if (event.damageSource.damageType !in listOf(DamageType.FALL, DamageType.STALAGMITE)) return
         val player = event.entity as? Player ?: return
         if (player.fallDistance >= distance) {
             trigger(player)
             return
         }
+    }
 
-        val fallDamage = event.damage
-        val world = player.world
-        val groundPos = player.location.clone().add(0.0, -0.1, 0.0)
-        val block = world.getBlockAt(groundPos)
-        val fallDistance = if (block.type == Material.POINTED_DRIPSTONE) {  // damage = (h-1)*2, h = damage/2+1
-            fallDamage / 2 + 1
-        } else fallDamage + 3
+    @EventHandler
+    fun onPlayerFallDamaged(event: PlayerMoveEvent) {
+        if(!event.hasExplicitlyChangedPosition()) return
 
-        if (fallDistance >= distance) {
-            trigger(player)
-        }
     }
 
     companion object {
