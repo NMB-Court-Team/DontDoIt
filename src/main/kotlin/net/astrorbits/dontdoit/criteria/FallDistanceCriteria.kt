ff9fa0408a0d2ee6e3e44b850bee81b470469a9b
Cookie_qwq <>
2025-09-26 20:08:19 +0800

    修FallDistance

--- src/main/kotlin/net/astrorbits/dontdoit/criteria/FallDistanceCriteria.kt
+++ src/main/kotlin/net/astrorbits/dontdoit/criteria/FallDistanceCriteria.kt
@@ -2,15 +2,14 @@ package net.astrorbits.dontdoit.criteria
 
 import net.astrorbits.dontdoit.criteria.helper.CriteriaType
 import net.astrorbits.dontdoit.system.team.TeamData
-import net.astrorbits.lib.math.vector.Vec3d
-import org.bukkit.Material
 import org.bukkit.damage.DamageType
 import org.bukkit.entity.Player
+import org.bukkit.event.Listener
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageEvent
 import kotlin.properties.Delegates
 
-class FallDistanceCriteria : Criteria() {
+class FallDistanceCriteria : Criteria(), Listener {
     override val type: CriteriaType = CriteriaType.FALL_DISTANCE
     var distance: Double by Delegates.notNull()
 
@@ -29,24 +28,12 @@ class FallDistanceCriteria : Criteria() {
     }
 
     @EventHandler
-    fun onPlayerDamaged(event: EntityDamageEvent) {  //TODO 还是有问题
-        if (event.damageSource.damageType != DamageType.FALL) return
+    fun onPlayerFallDamaged(event: EntityDamageEvent) {
+        if (event.damageSource.damageType !in listOf(DamageType.FALL, DamageType.STALAGMITE)) return
         val player = event.entity as? Player ?: return
         if (player.fallDistance >= distance) {
             trigger(player)
             return
-        }
-
-        val fallDamage = event.damage
-        val world = player.world
-        val groundPos = player.location.clone().add(0.0, -0.1, 0.0)
-        val block = world.getBlockAt(groundPos)
-        val fallDistance = if (block.type == Material.POINTED_DRIPSTONE) {  // damage = (h-1)*2, h = damage/2+1
-            fallDamage / 2 + 1
-        } else fallDamage + 3
-
-        if (fallDistance >= distance) {
-            trigger(player)
         }
     }
 
