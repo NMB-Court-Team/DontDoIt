e4bd35aa980e0966f9743aad3e914687acc15d08
LittleSadSheep <2749533087@qq.com>
2025-10-24 11:19:48 +0800

    fix: 创建onBlockPlace 监听玩家放置方块

--- src/main/kotlin/net/astrorbits/dontdoit/criteria/InteractBlockWithItemCriteria.kt
+++ src/main/kotlin/net/astrorbits/dontdoit/criteria/InteractBlockWithItemCriteria.kt
@@ -5,13 +5,18 @@ import net.astrorbits.dontdoit.criteria.inspect.BlockInspectCandidate
 import net.astrorbits.dontdoit.criteria.inspect.InventoryInspectContext
 import net.astrorbits.dontdoit.criteria.inspect.InventoryItemInspectCandidate
 import net.astrorbits.dontdoit.system.team.TeamData
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.UUID
 import kotlin.math.max
 
 class InteractBlockWithItemCriteria : Criteria(), Listener, BlockInspectCandidate, InventoryItemInspectCandidate {
@@ -48,16 +53,35 @@ class InteractBlockWithItemCriteria : Criteria(), Listener, BlockInspectCandidat
             this.isItemWildcard = isWildcard
         }
     }
+    private val recentBlockPlacers = mutableSetOf<UUID>()
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        recentBlockPlacers += event.player.uniqueId
+        val providingPlugin = JavaPlugin.getProvidingPlugin(InteractBlockWithItemCriteria::class.java)
+
+        Bukkit.getScheduler().runTaskLater(providingPlugin, Runnable {
+            recentBlockPlacers -= event.player.uniqueId
+        }, 1L)
+    }
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
     fun onInteractWithBlock(event: PlayerInteractEvent) {
         if (event.action != Action.RIGHT_CLICK_BLOCK) return
         val block = event.clickedBlock ?: return
         val item = event.item ?: ItemStack.empty()
 
         // if (event.useInteractedBlock() == Event.Result.DENY) return // 666神秘上游永远返回ALLOW诗人握持
+
         val player = event.player
-        if (player.isSneaking && item.type.isBlock) return //更改实现方法
+        // 排除“刚放置方块”的情况
+        if (player.uniqueId in recentBlockPlacers) return
+
+        // 允许 Paper 的潜行空手交互被触发
+        event.setUseInteractedBlock(Event.Result.ALLOW)
+        event.setUseItemInHand(Event.Result.ALLOW)
+
+
+        // if (player.isSneaking && item.type.isBlock) return // 更改实现方法
 
         if ((isBlockWildcard || block.type in blockTypes) &&
             (isItemWildcard || item.type in itemTypes)
